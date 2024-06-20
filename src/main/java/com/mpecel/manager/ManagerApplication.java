c85c0e7dd6953b8acabb679f1432ee366ec350c0
Michal Pecelerowicz <Michal.Pecelerowicz@ncbj.gov.pl>
2024-06-20 18:04:41 +0200

    Initial commit

---
+++ src/main/java/com/mpecel/manager/ManagerApplication.java
@@ -0,0 +1,13 @@
+package com.mpecel.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplicationgi
+public class ManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagerApplication.class, args);
+	}
+
+}
